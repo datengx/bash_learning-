0fc40923957f6ecbd0cd3a2d3f58376950819086
Da Teng <dateng@Das-MacBook-Air.local>
2013-04-07 17:05:59 -0500

    initial

---
+++ page.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# make_page - A script to produce an HTML file 
+
+echo "<HTML>"
+echo "<HEAD>"
+echo "  <TITLE>"
+echo "  The title of your page"
+echo "  </TITLE>"
+echo "</HEAD>"
+echo ""
+echo "<BODY>"
+echo "  Your page content goes here."
+echo "</BODY>"
+echo "</HTML>"
